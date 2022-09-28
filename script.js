025c3b04c61b4a1c5bf40dda4b4a9f866950b05d
DuilioAzevedo <duilio_uei@hotmail.com>
2022-09-28 15:25:43 -0300

    Form Values

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ const dateEl = document.getElementById('date-picker');
 // Set Date Input Min & Value with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+//Take Values from Form Inputs
+function updateCountdown(e) {
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+    console.log(countdownTitle, countdownDate);
+}
+
+// Event Listener
+countdownForm.addEventListener('submit', updateCountdown);
